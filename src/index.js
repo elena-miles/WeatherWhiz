b9d22c2f0dc6a30a3389dcd4954ce7b084578ac6
elena-miles <elena.airey@gmail.com>
2024-06-20 13:30:51 +0200

    adding Js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert( hello!);
+
+function handleSearchSubmit (event){
+event.preventDefault ();
+let SearchInput =document.querySelector ("#search-form-input");
+
+}
+
+let searchFormElement=document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
