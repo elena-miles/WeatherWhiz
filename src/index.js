e59531039331dc4c1609536adc77933f15e26b1a
elena-miles <elena.airey@gmail.com>
2024-06-20 15:03:05 +0200

    corrected file structure and started js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-
-alert(hi); 
-
-function handleSearchSubmit (event){
-event.preventDefault ();
-let SearchInput =document.querySelector ("#search-form-input");
-
-}
-
-let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+alert("you are a rotten egg");
