05515c707723805bebd21971ea5e2bf6e4416eff
elena-miles <elena.airey@gmail.com>
2024-06-23 23:13:24 +0200

    correct js and improve h1 upon testing

--- src/index.js
+++ src/index.js
@@ -6,22 +6,24 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input");
 
     let cityElement = document.querySelector("#city"); 
-    cityElement = searchInput.value;
-
     cityElement.innerHTML = searchInput.value; 
-}
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+    searchCity(searchInput.value);
+}
 
 function searchCity(city){
 // make api call and update interface
 let apiKey = "ffoa584b071af31b9db038336tec0bd6";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 console.log(apiURL);
+
+axios.get(apiURL).then(Response => {console.log(Response.data);
+document.querySelector('.display-degree-celcius').innerHTML = Response.data.temperature.current; 
+});
 }
 
-
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 
 
